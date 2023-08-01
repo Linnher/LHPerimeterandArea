e5311025cdb4a5dcb970ef8f16bd1f14ebebccb1
Linnher <107924149+Linnher@users.noreply.github.com>
2023-08-01 21:11:54 +1200

    Version 0 of AP Base - Skeleton Set Up

---
+++ 00_APBaseComponent_v0.py
@@ -0,0 +1,13 @@
+# Base component for the Area and Perimeter Program
+# Version Zero - Skeleton Setup
+
+# Author - Linn Herzig
+# LH 2023
+
+# Import libraries below this line **************************
+
+# Initiating lists below this line **************************
+
+# Functions below this line ******************************
+
+# Main Routine below this line ******************************
